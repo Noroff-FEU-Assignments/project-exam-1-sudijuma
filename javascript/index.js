bbd1cd331451887dbab60dee1cc853939dc4f25c
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-05-24 00:09:49 +0200

    updoot

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
+const API_URL = `https://mwakie.flywheelsites.com/wp-json/wp/v2/posts/`;
 
-const API_URL = `https://mwakie.flywheelsites.com/wp-json/wp/v2/media/`;
+const APIMEDIA_URL = `https://mwakie.flywheelsites.com/wp-json/wp/v2/media/`;
 const productDisplay = document.querySelector(".featured-post");
 async function displayPosts() {
   try {
@@ -7,11 +8,8 @@ async function displayPosts() {
     const productResponse = await productCall.json();
     for (let i = 0; i < productResponse.length; i++) {
       productDisplay.innerHTML += `      <div class="featured-post-container">
-      <div class="featured-post-image-container"><img src="${productResponse[i].source_url}" alt="" class="featured-image"></div>
+      <div class="featured-post-image-container"><img src="${productResponse[i].src}" alt="" srcset="${productResponse[i].srcset}" class="featured-image"></div>
       <h3 class="featured-post-headline">${productResponse[i].title.rendered}</h3>
-      <p class="featured-post-short-description">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
     </div>`;
     }
   } catch (e) {
