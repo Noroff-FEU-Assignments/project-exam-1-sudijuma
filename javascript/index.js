6c6c569eb11dc0a2f1d1ee7913ed2d438c2e79f2
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-05-23 17:00:34 +0200

    update

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,19 @@
 
-const API_URL = `https://mwakie.flywheelsites.com/wp-json/wc/posts`;
+const API_URL = `https://mwakie.flywheelsites.com/wp-json/wp/v2/posts/`;
 const productDisplay = document.querySelector(".featured-post");
-
 async function displayPosts() {
   try {
     const productCall = await fetch(API_URL);
     const productResponse = await productCall.json();
     for (let i = 0; i < productResponse.length; i++) {
-      console.log(productResponse[i])
-      productDisplay.innerHTML += ``;
+      productDisplay.innerHTML += `      <div class="featured-post-container">
+      <div class="featured-post-image-container"><img src="${productResponse[i].guid.rendered}" alt="" class="featured-image"></div>
+      <h3 class="featured-post-headline">${productResponse[i].title.rendered}</h3>
+      <p class="featured-post-short-description">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br />Quas,
+        dolorum?
+      </p>
+    </div>`;
     }
   } catch (e) {
     console.log(e);
