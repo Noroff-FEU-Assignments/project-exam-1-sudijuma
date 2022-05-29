c47aea043c21dfea302f96afdb04f99d85b51732
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-05-29 22:44:36 +0200

    contact form
    working on contact form

--- javascript/handler/validation.js
+++ javascript/handler/validation.js
@@ -1,14 +1,82 @@
-const yourName = document.getElementById("name").value;
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+const x = document.getElementById("name").value;
 const email = document.getElementById("email").value;
 const subject = document.getElementById("subject").value;
 const message = document.getElementById("message").value;
 const form = document.getElementById("form");
 const error = document.getElementById("error");
 
+/* function validateMe(e) { } */
 
-form.addEventListener("submit", (e) => {
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+/* button.onclick = function () {
+    const valid = input.value;
+    const validation
+} */
+messageContainer.onkeyup = function (event) {
+  const messageLength = event.target.value.length;
+  wordCount.innerHTML = messageLength;
+  if ((formSubmitBTN.Disabled = messageLength <= 9)) {
+  } else {
+    alert("message must be at least 10 characters");
+  }
+};
+
+function messageValidation() {
+  const x = document.forms["form"]["message"].value;
+  if (x <= 25) {
+    alert("At least 25 characters please");
+    return false;
+  } else {
+    return true;
+  }
+}
+function subjectValidation() {
+  const x = document.forms["form"]["subject"].value;
+  if (subject <= 15) {
+    alert("Input must be at least 15 characters long");
+    return false;
+  } else {
+    return true;
+  }
+}
+function nameValidation() {
+  const x = document.forms["form"]["name"].value;
+  if (x === "" || x === null) {
+    alert("Name required");
+    return false;
+  } else {
+    return true;
+  }
+}
+function emailValidation(email) {
+  var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.value.match(mailFormat)) {
+    alert("valid email adress!");
+    return true;
+  } else {
+    alert("invalid email");
+    return false;
+  }
+}
+
+/* form.addEventListener("submit", (e) => {
     let message = []
-    if (yourName === '' || yourName === null) {
+    if (x === '' || x === null) {
         message.push('Name is required')
     }
 
@@ -21,73 +89,4 @@ form.addEventListener("submit", (e) => {
         error.innerText = message.join(', ')
     }
     return true;
-})
-
-/* function validateMe(e) { } */
-
-function myFunction() {
-    var x = document.getElementById('myTopnav');
-    if (x.className === 'topnav') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'topnav';
-    }
-}
-
-
-function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
-/* button.onclick = function () {
-    const valid = input.value;
-    const validation
-} */
-messageContainer.onkeyup = function (event) {
-    const messageLength = event.target.value.length;
-    wordCount.innerHTML = messageLength;
-    if (formSubmitBTN.Disabled = messageLength <= 9) {
-    } else {
-        alert("message must be at least 10 characters")
-    }
-}
-
-
-function adressValidation(adressToCheck) {
-    if (adressToCheck <= 24) {
-        alert('Please enter valid address');
-        return false;
-    } else {
-        return true;
-    }
-}
-function subjectValidation(subject) {
-    if (subject <= 9) {
-        alert('Input must be at least 10 characters long');
-        return false;
-    } else {
-        return true;
-    }
-}
-function nameValidation(FandLname) {
-    if (FandLname == '') {
-        alert('Name required');
-        return false;
-    } else {
-        return true;
-    }
-}
-function emailValidation(email) {
-    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (email.value.match(mailFormat)) {
-        alert('valid email adress!');
-        return true;
-    } else {
-        alert('invalid email');
-        return false;
-    }
-}
-
+}) */
