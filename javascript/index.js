7665907e05afedac3dbab910c9a5bedac7eb186d
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-05-30 13:15:46 +0200

    just some small changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,3 @@
-/* const API_URL = `https://mwakie.flywheelsites.com/wp-json/wp/v2/posts/?_embed`;
-const productDisplay = document.querySelector(".featured-post");
-async function displayPosts() {
-  try {
-    const productCall = await fetch(API_URL);
-    const productResponse = await productCall.json();
-    console.log(productResponse);
-    for (let i = 0; i < productResponse.length; i++) {
-      productDisplay.innerHTML += `      <div class="featured-post-container">
-      <div class="featured-post-image-container"><img src="" alt="" srcset="" class="featured-image"></div>
-      <h3 class="featured-post-headline">${productResponse[i].title.rendered}</h3>
-      <a href="/specific.html?id=${productResponse[i].id}">View details</a>
-    </div>`;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
-displayPosts(); */
-
 const API_URL = `https://mwakie.flywheelsites.com/wp-json/wp/v2/posts/?_embed`;
 const productDisplay = document.querySelector(".blog-post-container");
 
@@ -34,7 +14,6 @@ fetch(API_URL)
       }
       const postTitle = apiResult[i].title.rendered;
       const postID = apiResult[i].id;
-      const postDate = apiResult[i].date;
       const postImg = apiResult[i]._embedded["wp:featuredmedia"][0].source_url;
       productDisplay.innerHTML += `
       <div class="blog-posts">
@@ -54,56 +33,3 @@ function myFunction() {
     x.className = 'topnav';
   }
 }
-/* const otherApiCall = `https://mwakie.flywheelsites.com/wp-json/wp/v2/posts/?_embed`;
-const postDisplay = document.querySelector(".sidebar-container");
-async function displayOtherPosts() {
-  try {
-    const productCall = await fetch(otherApiCall);
-    const productResponse = await productCall.json();
-    for (let i = 0; i < productResponse.length; i++) {
-      if (i > 3) {
-        return;
-      }
-      postDisplay.innerHTML += `
-      <div class="sidebar-container">
-      <div class="sidebar-content">
-      <div class="sidebar-image-container"><img src="${productResponse[i]._embedded['wp:featuredmedia'][0].source_url}" alt="" srcset="" class="sidebar-image" />
-      <a href="/specific.html?id=${productResponse[i].id}" class="link-container">${productResponse[i].title.rendered}</a>
-      </div>
-      </div>`;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
-displayOtherPosts() */
-
-
-/* 
-//1. select the input with id firstName
-
-const firstName = document.querySelector("#firstName");
-const errorContainer = document.querySelector(".error__message");
-const errorContainerSvg = document.getElementById("error-icon");
-//2. add an event to the input >>>> keyup onkeyup
-firstName.addEventListener("keyup", logLengthMsg);
-
-//3. Get the length of the input 
-function logLengthMsg(event){
-    const inputValue = event.target.value.trim();
-    const valueLength = inputValue.length;
-    if(valueLength > 0){
-        errorContainer.innerHTML = "the input has a value";
-        errorContainerSvg.style.display ="block";
-    }
-    else {
-        errorContainer.innerHTML = "The input dosent have a value";
-        errorContainerSvg.style.display ="block";
-    }
-}
-
-//4. make a condition 
-    //If the lentgh is > 0 
-        //Show message "the input has a value"
-    //else
-        //Show a message " the input dosent have a value" */
