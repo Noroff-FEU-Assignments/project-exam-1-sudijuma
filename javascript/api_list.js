0d4cd362000fa4cde80c9c11881b7517bb5dcf83
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-06-16 15:34:28 +0200

    Api
    Working on API list page

--- javascript/api_list.js
+++ javascript/api_list.js
@@ -6,3 +6,17 @@ function myFunction() {
         x.className = 'topnav';
     }
 }
+const displayApiImage = document.querySelector(".api-image-container");
+const nasaApi = 'https://api.nasa.gov/planetary/apod?api_key=bQda8zxBMGQUcbPIvQRKVczRhCkq1bJJDy5Jiq6V';
+async function displayApi() {
+    try {
+        const apiCall = await fetch(nasaApi);
+        const apiResponse = await apiCall.json();
+        console.log(apiResponse);
+        displayApiImage.innerHTML = `<div class="api-image-container"><img src="${apiResponse.url}" alt="" class="img-responsive"></div>
+        <div class="api-explanation-container"><p class="api-explanation">${apiResponse.explanation}</p></div></div>`
+    } catch (err) {
+        console.log(err);
+    }
+}
+displayApi();
